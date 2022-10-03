729a5c6162e19170035855118f09564b87ac2623
robbe beernaert <robbe.beernaert@student.howest.be>
2022-10-03 10:34:57 +0200

    added header comments of shaderCompiler

--- luna/src/core/utils/shaderCompiler.h
+++ luna/src/core/utils/shaderCompiler.h
@@ -24,15 +24,38 @@ namespace luna
 		class shaderCompiler
 		{
 		public:
+			/**
+			 * @brief constructor does nothing at the moment.
+			 * 
+			 */
 			shaderCompiler();
 			virtual ~shaderCompiler() {  };
+			/**
+			 * @brief compile a shader from source GLSL to specified.
+			 * 
+			 * \param compileSpec struct with compile specifications
+			 * \return compiled shadersource.
+			 */
 			std::vector<uint32_t> compile(compileSpec compileSpec);
 			
 
 		private:
+			/**
+			 * @brief reflects on the compiled shader and shows a text based dissasambly of the shader.
+			 * 
+			 * \param shaderData compiled shaderData.
+			 * \param reflect reflect yes or no.
+			 * \return param reflect.
+			 */
 			bool reflect(const std::vector<uint32_t>& shaderData, bool reflect);
+			/**
+			 * @brief returns the resource type name ex mat4 returns "Mat4".
+			 * 
+			 * \param resource to get the typename of.
+			 * \param compiler the compiler wich contains the compiled shader.
+			 * \return the type name of the resource as an std::string.
+			 */
 			std::string getResourceTypeName(const spirv_cross::Resource& resource, const spirv_cross::Compiler& compiler);
-			bool init = false;
 		};
 	}
 }
