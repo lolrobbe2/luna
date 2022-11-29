c6210f43e890d00090211139d9e452a05001b734
robbe beernaert <robbe.beernaert@student.howest.be>
2022-11-29 19:20:28 +0100

    added renderer2DData struct

--- luna/src/core/rendering/renderer2D.cpp
+++ luna/src/core/rendering/renderer2D.cpp
@@ -5,8 +5,14 @@ namespace luna
 	{
 		struct renderer2DData
 		{
+			const uint32_t maxQuads = 2000;
+			const uint32_t maxVertices = maxQuads * 4;
+			const uint32_t maxIndices = maxQuads * 6;
+			ref<vertexArray> vertexArray;
 
 		};
+
+		static renderer2DData rendererData;
 
 	}
 }
