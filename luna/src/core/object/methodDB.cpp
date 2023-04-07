f68c06517f3bacf67af4daa9439c8d8adc09f82e
robbe beernaert <robbe.beernaert@student.howest.be>
2023-04-07 08:58:57 +0200

    added externall method pointer

--- luna/src/core/object/methodDB.cpp
+++ luna/src/core/object/methodDB.cpp
@@ -2,13 +2,38 @@
 #include <core/scripting/scriptingEngine.h>
 #define LN_ADD_INTERNAL_CALL(Class,Function) luna::methodDB::bindInternalFunction<Class>(#Function,Function);
 #define LN_ADD_EXTERNAL_CALL(Class,Function) //TODO
+
+#ifndef TYPED_METHOD_BIND
+class __UnexistingClass;
+#define MB_T __UnexistingClass
+#else
+#define MB_T T
+#endif
+
+
+
 namespace luna
 {
+	#ifdef TYPED_METHOD_BIND
+		template <class T, class... P>
+	#else
+		template <class... P>
+	#endif
+	struct Method
+	{
+		void (MB_T::*method)(P...) = nullptr;
+		int argsCount;
+	};
+
 	struct methodDBData
 	{
 		//TODO change map with hashed objStorage.
-		
+		std::map<std::string,Method<>> methodMap;
 	};
+
+	static s_Data = new methodDBData();
+
+	void methodDB::init();
 
 	void methodDB::bindFunctions()
 	{
