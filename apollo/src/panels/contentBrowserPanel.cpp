2d2584e809dcb4a386866b63ec930fb6a69bcfe3
robbe beernaert <robbebeenaert@hotmail.be>
2023-06-09 21:05:50 +0200

    added icon resources

--- apollo/src/panels/contentBrowserPanel.cpp
+++ apollo/src/panels/contentBrowserPanel.cpp
@@ -2,17 +2,43 @@
 #include <core/vulkan/rendering/vulkanTexture.h>
 namespace luna
 {
+
 	contentBrowserPanel::contentBrowserPanel()
 	{
 		m_BaseDirectory = std::filesystem::current_path();
 		m_CurrentDirectory = m_BaseDirectory;
 		assets::assetManager::importAsset("src/resources/directoryIcon.png",assets::texture);
+		assets::assetManager::importAsset("src/resources/directoryIconHovered.png", assets::texture);
+		
+		assets::assetManager::importAsset("src/resources/icon_png.png", assets::texture);
+		assets::assetManager::importAsset("src/resources/icon_png_hovered.png", assets::texture);
+		
+		assets::assetManager::importAsset("src/resources/icon_ttf.png", assets::texture);
+		assets::assetManager::importAsset("src/resources/icon_ttf_hovered.png", assets::texture);
+
 		assets::assetManager::importAsset("src/resources/fileIcon.png", assets::texture);
 
+
 		directoryIcon = assets::assetManager::getAsset("directoryIcon.png");
+		directoryHoveredIcon = assets::assetManager::getAsset("directoryIconHovered.png");
+		
+		pngIcon = assets::assetManager::getAsset("icon_png.png");
+		pngHoveredIcon = assets::assetManager::getAsset("icon_png_hovered.png");
+
+		ttfIcon = assets::assetManager::getAsset("icon_ttf.png");
+		ttfHoveredIcon = assets::assetManager::getAsset("icon_ttf_hovered.png");
+		
 		fileIcon = assets::assetManager::getAsset("fileIcon.png");
 		
 		std::dynamic_pointer_cast<vulkan::vulkanTexture>(directoryIcon)->createGuiImage();
+		std::dynamic_pointer_cast<vulkan::vulkanTexture>(directoryHoveredIcon)->createGuiImage();
+
+		std::dynamic_pointer_cast<vulkan::vulkanTexture>(pngIcon)->createGuiImage();
+		std::dynamic_pointer_cast<vulkan::vulkanTexture>(pngHoveredIcon)->createGuiImage();
+
+		std::dynamic_pointer_cast<vulkan::vulkanTexture>(ttfIcon)->createGuiImage();
+		std::dynamic_pointer_cast<vulkan::vulkanTexture>(ttfHoveredIcon)->createGuiImage();
+		
 		std::dynamic_pointer_cast<vulkan::vulkanTexture>(fileIcon)->createGuiImage();
 	}
 	void contentBrowserPanel::onImGuiRender()
@@ -40,15 +66,21 @@ namespace luna
 
 			for (auto& directoryEntry : std::filesystem::directory_iterator(m_CurrentDirectory))
 			{
+				static std::string hovered;
 				const auto& path = directoryEntry.path();
 				std::string filenameString = path.filename().string();
 
 				ImGui::PushID(filenameString.c_str());
 
-				ref<vulkan::vulkanTexture> icon = directoryEntry.is_directory() ? std::dynamic_pointer_cast<vulkan::vulkanTexture>(directoryIcon) : std::dynamic_pointer_cast<vulkan::vulkanTexture>(fileIcon);
+				ref<vulkan::vulkanTexture> icon = std::dynamic_pointer_cast<vulkan::vulkanTexture>(getIcon(directoryEntry, hovered == filenameString));
 
 
-				ImGui::ImageButton(icon->getGuiImageHandle(), {thumbnailSize, thumbnailSize}, {1, 0}, {0, 1});
+				ImGui::ImageButton(icon->getGuiImageHandle(), {thumbnailSize, thumbnailSize}, {0, 0}, {1, 1});
+				if (ImGui::IsItemHovered()) 
+					hovered = filenameString;
+				else if (hovered == filenameString)
+					hovered = "";
+
 				if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
 				{
 					if (directoryEntry.is_directory())
@@ -63,5 +95,22 @@ namespace luna
 			}
 		}
 		ImGui::End();
+	}
+	ref<assets::asset> contentBrowserPanel::getIcon(const std::filesystem::directory_entry directoryEntry, bool hovered)
+	{
+		if (directoryEntry.is_directory())
+		{
+			if (hovered) return directoryHoveredIcon;
+			return directoryIcon;
+		}
+		else if (directoryEntry.path().extension().string() == ".png") {
+			if (hovered) return  pngHoveredIcon;
+			return pngIcon;
+		}
+		else if (directoryEntry.path().extension().string() == ".ttf") {
+			if (hovered) return  ttfHoveredIcon;
+			return ttfIcon;
+		}
+		return fileIcon;
 	}
 }
--- apollo/src/panels/contentBrowserPanel.h
+++ apollo/src/panels/contentBrowserPanel.h
@@ -9,9 +9,20 @@ namespace luna
 		contentBrowserPanel();
 		void onImGuiRender();
 	private:
+		ref<assets::asset> getIcon(const std::filesystem::directory_entry directoryEntry, bool hovered);
+
+
 		std::filesystem::path m_BaseDirectory;
 		std::filesystem::path m_CurrentDirectory;
 		ref<assets::asset> directoryIcon;
+		ref<assets::asset> directoryHoveredIcon;
+
+		ref<assets::asset> pngIcon;
+		ref<assets::asset> pngHoveredIcon;
+
+		ref<assets::asset> ttfIcon;
+		ref<assets::asset> ttfHoveredIcon;
+
 		ref<assets::asset> fileIcon;
 
 	};
