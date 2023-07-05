8dd6a34cc0598da48f266702c6295908ccb9f69d
lolrobbe2 <90089980+lolrobbe2@users.noreply.github.com>
2023-07-05 16:30:23 +0200

    Revert "merge assetManager + Project"

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -8,12 +8,7 @@ class sandbox : public luna::application::application
 public:
 	sandbox() //base function
 	{
-		LN_TRACE("started app");
-		luna::assets::editorAssetManager* assetManager = new luna::assets::editorAssetManager();
-		assetManager->importAsset("src/assets/media/statue.png", luna::assets::texture);
-		assetManager->importAsset("src/assets/media/Roboto-Regular.ttf", luna::assets::font); 
-		assetManager->loadImportedAssetsMetadata();
-    
+	
 		//luna::project::projectManager::init();
 	}
 	virtual ~sandbox() //base breaker function
