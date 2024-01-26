42d585c074e45c9ed3e09b8584aaa03cc328cd89
Beernaert Robbe <robbe.beernaert@student.howest.be>
2024-01-26 18:47:34 +0100

    fixed the allocator problem, i think!

--- luna/src/core/assets/assetImporter.cpp
+++ luna/src/core/assets/assetImporter.cpp
@@ -6,11 +6,22 @@ namespace luna
 {
     namespace assets
     {
+        static ref<artemis::allocator> p_allocator;
+
         using AssetImportFunction = std::function<ref<asset>(assetHandle, assetMetadata*)>;
+
         static std::map<assetType, AssetImportFunction> s_assetImportFunctions = {
             { assetType::texture, textureImporter::importTexture },
             { assetType::font   , fontImporter::importFont       },
         };
+        void assetImporter::setAllocator(const ref<artemis::allocator> p_allocator)
+        {
+            assets::p_allocator = p_allocator;
+        }
+        const ref<artemis::allocator> assetImporter::getAllocator()
+        {
+            return p_allocator;
+        }
         ref<asset> assetImporter::importAsset(assetHandle handle,assetMetadata* metadata)
         {
             LN_ERR_FAIL_COND_V_MSG(s_assetImportFunctions.find(metadata->assetType) == s_assetImportFunctions.end(), nullptr, "No importer available for asset type!");
