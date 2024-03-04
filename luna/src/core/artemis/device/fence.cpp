f35758ce3dc46ffe4f9f3a01ad5311e045d623b2
Beernaert Robbe <robbe.beernaert@student.howest.be>
2024-03-04 11:44:01 +0100

    improvements where made!

--- luna/src/core/artemis/device/fence.cpp
+++ luna/src/core/artemis/device/fence.cpp
@@ -5,6 +5,26 @@ namespace luna
 {
 	namespace artemis 
 	{
+		void fence::wait(bool waitForAll, uint64_t timeOut)
+		{
+			vkWaitForFences(*p_device, 1, &_fence, waitForAll ? VK_TRUE : VK_FALSE, timeOut);
+		}
+		void fence::wait(std::vector<fence> fences, bool waitForAll, uint64_t timeOut)
+		{
+			std::vector<VkFence> nativeFences;
+			nativeFences.resize(fences.size(), fences[nativeFences.size()].setNoDestroy(true).native());
+			vkWaitForFences(*fences[0].p_device, nativeFences.size(), nativeFences.data(), waitForAll, timeOut);
+		}
+		void fence::reset()
+		{
+			vkResetFences(*p_device, 1, &_fence);
+		}
+		void fence::reset(std::vector<fence> fences)
+		{
+			std::vector<VkFence> nativeFences;
+			nativeFences.resize(fences.size(), fences[nativeFences.size()].setNoDestroy(true).native());
+			vkResetFences(*fences[0].p_device, fences.size(),nativeFences.data());
+		}
 		fence::fence(const VkDevice* device,const VkFenceCreateFlags flags)
 		{
 			VkFenceCreateInfo info{ VK_STRUCTURE_TYPE_FENCE_CREATE_INFO };
@@ -12,7 +32,7 @@ namespace luna
 			info.pNext = nullptr;
 			VkResult res = vkCreateFence(*device, &info, nullptr, &_fence);
 			LN_ERR_FAIL_COND_MSG(res != VK_SUCCESS, "[Artemis] an error occured while trying to create fence, VkResult: " + VK_RESULT(res));
-			_device = device;
+			p_device = device;
 		}
 	}
 } 
--- luna/src/core/artemis/device/fence.h
+++ luna/src/core/artemis/device/fence.h
@@ -12,12 +12,21 @@ namespace luna
 			operator const VkFence() const { return _fence; }
 			operator const VkFence*() const { return &_fence; }
 			const VkFence native() { return _fence; }
-			~fence() { if(_fence != VK_NULL_HANDLE)vkDestroyFence(*_device, _fence, nullptr); }
+			~fence() { if(_fence != VK_NULL_HANDLE && !noDestroy)vkDestroyFence(*p_device, _fence, nullptr); }
+			_ALWAYS_INLINE_ void wait(bool waitForAll = true, uint64_t timeOut = UINT64_MAX);
+			_ALWAYS_INLINE_ static void wait(std::vector<fence> fences, bool waitForAll = true, uint64_t timeOut = UINT64_MAX);
+			_ALWAYS_INLINE_ void reset();
+			_ALWAYS_INLINE_ static void reset(std::vector<fence> fences);
+			/**
+			* @brief when the deconstructor is called en noDestroy is true then the native fence (VkFence) will not be destroyed;
+			*/
+			_ALWAYS_INLINE_ fence setNoDestroy(bool noDestroy = false) { noDestroy = true; return *this; }
 		protected: 
 			friend class device;
 			fence(const VkDevice* device, const VkFenceCreateFlags flags);
 		private:
-			const VkDevice* _device;
+			const VkDevice* p_device;
+			bool noDestroy = false;
 			VkFence _fence = VK_NULL_HANDLE;
 		};
 	}
