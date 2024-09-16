f5feb7c218ef40466c95a215c8d49a98b7e217ea
Beernaert Robbe <robbe.beernaert@student.howest.be>
2024-09-16 14:16:43 +0200

    started work on hashedObjectStorage for storing methods

---
+++ luna/src/core/utils/hashedObjectStorage.h
@@ -0,0 +1,46 @@
+#pragma once
+#ifndef _HASHED_OBJECT_STORAGE_
+#define _HASHED_OBJECT_STORAGE_
+#include "objectStorage.h"
+namespace luna
+{
+	namespace utils
+	{
+		using storageObject = uint64_t;
+		enum storageResult
+		{
+			storageInvalidHandle = 0,
+			storageOpSucces = 1,
+			storageOpFailed = 2
+		};
+
+		template<typename keyObject, typename value>
+		class hashedObjectStorage
+		{
+		public:
+			// Default constructor with default cache size of 200
+			hashedObjectStorage() : objectStorage(200) {}
+
+			// Constructor with custom cache size
+			hashedObjectStorage(size_t cacheSize) : objectStorage(cacheSize) {}
+			_ALWAYS_INLINE_ std::pair<storageResult, value> putValue(const keyObject& key, const value&& _value)
+			{
+				storage.putValue(hashFunction(key),std::forward<value>(_value))
+			}
+			_ALWAYS_INLINE_ value getValue(const keyObject& key)
+			{
+				return storage.getValue(hashFunction(key)).second;
+			}
+		private:
+			uint64_t hashFunction(const key& key) const
+			{
+				static_assert(std::is_default_constructible_v<std::hash<keyObject>>,
+					"[hashedObjectStorage]keyObject must be hashable using std::hash.");
+				return std::hash<keyObject>{}(key);
+			}
+			objectStorage<value> storage;
+		};
+	}
+}
+#endif //!_HASHED_OBJECT_STORAGE_
+
